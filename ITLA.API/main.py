2ef1a1d34c40b2e2bd5a860ac1c5c96f4f6c551d
Wordy <wordymarte848@gmail.com>
2025-06-23 12:55:06 -0400

    login-50%

--- ITLA.API/main.py
+++ ITLA.API/main.py
@@ -22,7 +22,6 @@ app.add_middleware(
 app.include_router(auth.router, prefix="/internal")
 
 # Servir archivos estáticos
-app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
-
+app.mount("/", StaticFiles(directory="Frontend", html=True), name="Frontend")
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
